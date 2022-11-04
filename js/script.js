dfe13b20284e92fbd9f14c42c09e65ae99563b94
Ran <rsheridan613@gmail.com>
2022-11-03 20:43:28 -0600

    tweaked css, made variables to condence js

--- js/script.js
+++ js/script.js
@@ -1,112 +1,111 @@
-window.onload = function () {
-  var questions = [
-    {
-      ask: "Question1",
-      answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
-      right: "Answer 2",
-    },
-    {
-      ask: "Question2",
-      answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
-      right: "Answer 1",
-    },
-    {
-      ask: "Question3",
-      answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
-      right: "Answer 4",
-    },
-  ];
-  var secondsLeft = 30;
-  var start = document.getElementById("start");
-  var view = document.getElementById("view");
-  var timeEl = document.getElementById("timer");
-  var question = document.getElementById("question");
-  var answers = document.getElementById("answers");
-  var message = document.getElementById("message");
-  var highscore = 0;
-  var questionNumber = 0;
-  var rightAnswer = "";
-  var userAnswer = "";
-  let timerInterval;
+var questions = [
+  {
+    ask: "How many eyes does a bee have?",
+    answers: ["2", "5", "8", "Over 100"],
+    right: "5",
+  },
+  {
+    ask: "How long have humans kept bees?",
+    answers: ["< 100 years", "500 years", "2156 years", "> 4000 years"],
+    right: "> 4000 years",
+  },
+  {
+    ask: "The honeybee is the official insect of which state?",
+    answers: ["California", "Georgia", "Maine", "Oregon"],
+    right: "Maine",
+  },
+];
+var secondsLeft = 45;
+var startEl = document.getElementById("start");
+var quizEl = document.getElementById("quiz");
+var scoreboardEl = document.getElementById("scoreboard");
+var viewEl = document.getElementById("view");
+var timeEl = document.getElementById("timer");
+var question = document.getElementById("question");
+var answers = document.getElementById("answers");
+var message = document.getElementById("message");
+var highscore = 0;
+var questionNumber = 0;
+var rightAnswer = "";
+var userAnswer = "";
+let timerInterval;
 
-  start.addEventListener("click", startQuiz);
-  view.addEventListener("click", showScores);
+startEl.addEventListener("click", startQuiz);
+view.addEventListener("click", showScores);
 
-  // On start click
-  function startQuiz() {
-    document.getElementById("start").classList.add("hide");
-    document.getElementById("quiz").classList.remove("hide");
-    // start.removeEventListener("click");
-    question.textContent = "";
-    answers.textContent = "";
+// On start click
 
-    startTimer();
+function startQuiz() {
+  startEl.classList.add("hide");
+  quizEl.classList.remove("hide");
+  question.textContent = "";
+  answers.textContent = "";
+
+  startTimer();
+  nextQuestion(questions);
+}
+
+//Next question
+
+function nextQuestion(array) {
+  if (questionNumber < array.length) {
+    rightAnswer = array[questionNumber].right;
+    question.innerText = array[questionNumber].ask;
+    var choices = array[questionNumber].answers;
+
+    for (let index = 0; index < choices.length; index++) {
+      const element = choices[index];
+      var answerButton = document.createElement("button");
+      answerButton.setAttribute("class", "answer");
+      answerButton.textContent = element;
+      answers.append(answerButton);
+    }
+  } else {
+    highscore = secondsLeft;
+    secondsLeft = 0;
+    console.log(highscore);
+    showScores();
+  }
+}
+
+//Check answer
+
+document.addEventListener("click", function (event) {
+  if (event.target && event.target.matches(".answer")) {
+    userAnswer = event.target.textContent;
+    console.log(userAnswer);
+    if (userAnswer === rightAnswer) {
+      message.textContent = "Right Answer!";
+    } else {
+      message.textContent = "Wrong Answer!";
+      secondsLeft -= 10;
+    }
+    questionNumber++;
+    question.innerHTML = "";
+    answers.innerHTML = "";
     nextQuestion(questions);
   }
+});
 
-  //Next question
+//Show scores
 
-  function nextQuestion(array) {
-    if (questionNumber < array.length) {
-      rightAnswer = array[questionNumber].right;
-      question.innerText = array[questionNumber].ask;
-      var choices = array[questionNumber].answers;
+function showScores() {
+  startEl.classList.add("hide");
+  quizEl.classList.add("hide");
+  scoreboardEl.classList.remove("hide");
+}
 
-      for (let index = 0; index < choices.length; index++) {
-        const element = choices[index];
-        var answerButton = document.createElement("button");
-        answerButton.setAttribute("class", "answer");
-        answerButton.textContent = element;
-        answers.append(answerButton);
-      }
+//Timer
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if (secondsLeft < 1) {
+      clearInterval(timerInterval);
+      console.log("game over");
     } else {
-      highscore = secondsLeft;
-      secondsLeft = 0;
-      console.log(highscore);
-      message.textContent = "Game Over";
+      secondsLeft--;
     }
-  }
-
-  document.addEventListener("click", function (event) {
-    event.preventDefault();
-    event.stopImmediatePropagation();
-    if (event.target && event.target.matches(".answer")) {
-      userAnswer = event.target.textContent;
-      console.log(userAnswer);
-      if (userAnswer === rightAnswer) {
-        message.textContent = "Right Answer!";
-      } else {
-        message.textContent = "Wrong Answer!";
-        secondsLeft -= 10;
-      }
-      questionNumber++;
-      // setTimeout(() => {
-      question.innerHTML = "";
-      answers.innerHTML = "";
-      nextQuestion(questions);
-      // }, 1000);
-    }
-  });
-
-  //Check answer
-  function checkAnswer() {}
-
-  //Show scores
-  function showScores() {
-    console.log("scores");
-  }
-
-  //Timer
-  function startTimer() {
-    timerInterval = setInterval(() => {
-      timeEl.textContent = "Time: " + secondsLeft;
-
-      if (secondsLeft < 1) {
-        clearInterval(timerInterval);
-        console.log("game over");
-      } else {
-        secondsLeft--;
-      }
-    }, 1000);
-  }
-};
+  }, 1000);
+}
